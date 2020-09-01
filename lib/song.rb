4f1b7e257af0e6265c93953079f0c9a62acad0bf
Rafael Garcia <rafagrcb@gmail.com>
2020-09-01 18:54:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
 
   def initialize(name)
     @name = name
-    @@
+    @@all << self
   end
 
   def self.new_by_filename(file)
@@ -22,7 +22,7 @@ class Song
   end
   
   def self.all
-    return 
+    @@all
   end
   
  end
